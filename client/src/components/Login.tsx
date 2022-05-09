82cc5afee0a2426fc70f9440da6645876c48bb25
Soomin Jang <jsmsumin2@gmail.com>
2022-05-10 04:35:48 +0900

    dropSchema: true

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -51,7 +51,6 @@ const Login: NextPage = () => {
 					// 액세스 토큰과 리프레쉬 토큰을 var 에 담아두기
 					accessTokenVar(res.data.accessToken)
 					refreshTokenVar(res.data.refereshToken)
-
 					// ! 여기서 유저나 트레이너 페이지로 이동할 때 해당 유저의 정보를 받아서 이동 (app.tsx에서)
 					router.push(res.data.redirectUrl)
 				})
@@ -83,20 +82,20 @@ const Login: NextPage = () => {
 					/>
 					<button
 						onClick={onSubmit}
-						className={`w-full h-[4.8rem] mt-[2rem] text-black bg-[#FDAD00] cursor-pointer disabled:opacity-50 rounded-[2rem]`}
-					>
+						className={`w-full h-[4.8rem] mt-[2rem] text-black bg-[#FDAD00] cursor-pointer disabled:opacity-50 rounded-[2rem]`}>
 						로그인
 					</button>
 					<button
 						onClick={onGoogleLogin}
-						className={`w-full h-[4.8rem] mt-[0.7rem] text-black bg-gray-200 cursor-pointer disabled:opacity-50 rounded-[2rem]`}
-					>
+						className={`w-full h-[4.8rem] mt-[0.7rem] text-black bg-gray-200 cursor-pointer disabled:opacity-50 rounded-[2rem]`}>
 						GOOGLE로 로그인
 					</button>
 					<div className="flex ml-[1rem]">
 						<div className="flex border-0 mt-[2rem]">처음이신가요?</div>
 						<Link href="/signup" passHref>
-							<div className="underline mt-[2rem] text-[#FDAD00] ml-[0.5rem]">회원가입</div>
+							<div className="underline mt-[2rem] text-[#FDAD00] ml-[0.5rem]">
+								회원가입
+							</div>
 						</Link>
 					</div>
 				</div>
