b795df7e2bbd1a14d645f09a54333f38261875e7
Soomin Jang <jsmsumin2@gmail.com>
2022-05-09 19:58:41 +0900

    수업 완료에 따른 sessionHistory 데이터 변경

--- server/src/session-histories/session-histories.service.ts
+++ server/src/session-histories/session-histories.service.ts
@@ -1,149 +1,150 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Trainer } from 'src/trainers/entities/trainer.entity';
-import { TrainersService } from 'src/trainers/trainers.service';
-import { User } from 'src/users/entities/user.entity';
-import { UsersService } from 'src/users/users.service';
-import { Repository } from 'typeorm';
-import { CreateSessionHistoryInput } from './dto/create-session-history.input';
-import { UpdateSessionHistoryInput } from './dto/update-session-history.input';
-import { SessionHistory } from './entities/session-history.entity';
+import { forwardRef, Inject, Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Trainer } from 'src/trainers/entities/trainer.entity'
+import { TrainersService } from 'src/trainers/trainers.service'
+import { User } from 'src/users/entities/user.entity'
+import { UsersService } from 'src/users/users.service'
+import { Repository } from 'typeorm'
+import { CreateSessionHistoryInput } from './dto/create-session-history.input'
+import { UpdateSessionHistoryInput } from './dto/update-session-history.input'
+import { SessionHistory } from './entities/session-history.entity'
 
 @Injectable()
 export class SessionHistoriesService {
-  constructor(
-    @InjectRepository(SessionHistory)
-    private sessionHistoriesRepository: Repository<SessionHistory>,
-    @Inject(forwardRef(() => UsersService)) private usersService: UsersService,
-    @Inject(forwardRef(() => TrainersService))
-    private trainersService: TrainersService,
-  ) {}
+	constructor(
+		@InjectRepository(SessionHistory)
+		private sessionHistoriesRepository: Repository<SessionHistory>,
+		@Inject(forwardRef(() => UsersService))
+		private usersService: UsersService,
+		@Inject(forwardRef(() => TrainersService))
+		private trainersService: TrainersService
+	) {}
 
-  async create(
-    createSessionHistoryInput: CreateSessionHistoryInput,
-  ): Promise<SessionHistory> {
-    const user = await this.usersService.findOneById(
-      createSessionHistoryInput.userId,
-    );
-    const createParams = {
-      ...createSessionHistoryInput,
-      trainerId: user.trainerId,
-    };
-    const newSessionHistory =
-      this.sessionHistoriesRepository.create(createParams);
-    return this.sessionHistoriesRepository.save(newSessionHistory);
-  }
+	async create(
+		createSessionHistoryInput: CreateSessionHistoryInput
+	): Promise<SessionHistory> {
+		const user = await this.usersService.findOneById(
+			createSessionHistoryInput.userId
+		)
+		const createParams = {
+			...createSessionHistoryInput,
+			trainerId: user.trainerId
+		}
+		const newSessionHistory =
+			this.sessionHistoriesRepository.create(createParams)
+		return this.sessionHistoriesRepository.save(newSessionHistory)
+	}
 
-  findAll(): Promise<SessionHistory[]> {
-    return this.sessionHistoriesRepository.find();
-  }
+	findAll(): Promise<SessionHistory[]> {
+		return this.sessionHistoriesRepository.find()
+	}
 
-  findOneById(id: number): Promise<SessionHistory> {
-    return this.sessionHistoriesRepository.findOneOrFail(id);
-  }
+	findOneById(id: number): Promise<SessionHistory> {
+		return this.sessionHistoriesRepository.findOneOrFail(id)
+	}
 
-  findAllByUserId(userId: number): Promise<SessionHistory[]> {
-    return this.sessionHistoriesRepository.find({ userId });
-  }
+	findAllByUserId(userId: number): Promise<SessionHistory[]> {
+		return this.sessionHistoriesRepository.find({ userId })
+	}
 
-  findAllByTrainerId(trainerId: number): Promise<SessionHistory[]> {
-    return this.sessionHistoriesRepository.find({ trainerId });
-  }
+	findAllByTrainerId(trainerId: number): Promise<SessionHistory[]> {
+		return this.sessionHistoriesRepository.find({ trainerId })
+	}
 
-  async findOneToAddUsedCountByUserIdAndTrainerId(
-    userId: number,
-    trainerId: number,
-  ): Promise<SessionHistory> {
-    return await this.sessionHistoriesRepository
-      .createQueryBuilder('sessionHistory')
-      .where(
-        'sessionHistory.trainerId = :trainerId AND sessionHistory.userId = :userId',
-        { userId, trainerId },
-      )
-      .where('sessionHistory.totalCount - sessionHistory.usedCount > 0')
-      .orderBy('sessionHistory.createdAt', 'ASC')
-      .take(1)
-      .getOne();
-  }
+	async findOneToAddUsedCountByUserIdAndTrainerId(
+		userId: number,
+		trainerId: number
+	): Promise<SessionHistory> {
+		return await this.sessionHistoriesRepository
+			.createQueryBuilder('sessionHistory')
+			.where(
+				'sessionHistory.trainerId = :trainerId AND sessionHistory.userId = :userId',
+				{ userId, trainerId }
+			)
+			.orderBy('sessionHistory.createdAt', 'ASC')
+			.take(1)
+			.getOne()
+	}
 
-  //  usedCount != totalCount인 히스토리중 가장 먼저 생성된 것을 찾는다
-  //  - 이 세션 히스토리의 usedCount > 0 일 경우 이 히스토리에서 usedCount를 -1 한다
-  //  - 이 세션 히스토리의 usedCount = 0 일 경우, 가장 최신의 usedCount = totalCount 세션 히스토리를 찾아 여기에서 -1을 한다
-  async findOneToSubtractUsedCountByUserIdAndTrainerId(
-    userId: number,
-    trainerId: number,
-  ): Promise<SessionHistory> {
-    const sessionHistory = await this.sessionHistoriesRepository
-      .createQueryBuilder('sessionHistory')
-      .where(
-        'sessionHistory.trainerId = :trainerId AND sessionHistory.userId = :userId',
-        { userId, trainerId },
-      )
-      .where('sessionHistory.usedCount != sessionHistory.totalCount')
-      .orderBy('sessionHistory.createdAt', 'ASC')
-      .take(1)
-      .getOne();
+	//  usedCount != totalCount인 히스토리중 가장 먼저 생성된 것을 찾는다
+	//  - 이 세션 히스토리의 usedCount > 0 일 경우 이 히스토리에서 usedCount를 -1 한다
+	//  - 이 세션 히스토리의 usedCount = 0 일 경우, 가장 최신의 usedCount = totalCount 세션 히스토리를 찾아 여기에서 -1을 한다
+	async findOneToSubtractUsedCountByUserIdAndTrainerId(
+		userId: number,
+		trainerId: number
+	): Promise<SessionHistory> {
+		const sessionHistory = await this.sessionHistoriesRepository
+			.createQueryBuilder('sessionHistory')
+			.where(
+				'sessionHistory.trainerId = :trainerId AND sessionHistory.userId = :userId',
+				{ userId, trainerId }
+			)
+			.orderBy('sessionHistory.createdAt', 'ASC')
+			.take(1)
+			.getOne()
 
-    if (sessionHistory && sessionHistory.usedCount > 0) {
-      return sessionHistory;
-    }
+		if (sessionHistory && sessionHistory.usedCount > 0) {
+			return sessionHistory
+		}
 
-    return await this.sessionHistoriesRepository
-      .createQueryBuilder('sessionHistory')
-      .where(
-        'sessionHistory.trainerId = :trainerId AND sessionHistory.userId = :userId',
-        { userId, trainerId },
-      )
-      .where('sessionHistory.usedCount = sessionHistory.totalCount')
-      .orderBy('sessionHistory.createdAt', 'DESC')
-      .take(1)
-      .getOne();
-  }
+		return await this.sessionHistoriesRepository
+			.createQueryBuilder('sessionHistory')
+			.where(
+				'sessionHistory.trainerId = :trainerId AND sessionHistory.userId = :userId',
+				{ userId, trainerId }
+			)
+			.where('sessionHistory.usedCount = sessionHistory.totalCount')
+			.orderBy('sessionHistory.createdAt', 'DESC')
+			.take(1)
+			.getOne()
+	}
 
-  getUser(userId: number): Promise<User> {
-    return this.usersService.findOneById(userId);
-  }
+	getUser(userId: number): Promise<User> {
+		return this.usersService.findOneById(userId)
+	}
 
-  getTrainer(trainerId: number): Promise<Trainer> {
-    return this.trainersService.findOneById(trainerId);
-  }
+	getTrainer(trainerId: number): Promise<Trainer> {
+		return this.trainersService.findOneById(trainerId)
+	}
 
-  async update(
-    updateSessionHistoryInput: UpdateSessionHistoryInput,
-  ): Promise<SessionHistory> {
-    const sessionHistory = await this.sessionHistoriesRepository.findOneOrFail(
-      updateSessionHistoryInput.id,
-    );
-    return this.sessionHistoriesRepository.save({
-      ...sessionHistory,
-      ...updateSessionHistoryInput,
-    });
-  }
+	async update(
+		updateSessionHistoryInput: UpdateSessionHistoryInput
+	): Promise<SessionHistory> {
+		const sessionHistory =
+			await this.sessionHistoriesRepository.findOneOrFail(
+				updateSessionHistoryInput.id
+			)
+		return this.sessionHistoriesRepository.save({
+			...sessionHistory,
+			...updateSessionHistoryInput
+		})
+	}
 
-  async remove(id: number): Promise<SessionHistory> {
-    const sessionHistory = await this.sessionHistoriesRepository.findOneOrFail(
-      id,
-    );
-    return this.sessionHistoriesRepository.remove(sessionHistory);
-  }
+	async remove(id: number): Promise<SessionHistory> {
+		const sessionHistory =
+			await this.sessionHistoriesRepository.findOneOrFail(id)
+		return this.sessionHistoriesRepository.remove(sessionHistory)
+	}
 
-  /**
-   *
-   * Helpers
-   *
-   */
+	/**
+	 *
+	 * Helpers
+	 *
+	 */
 
-  async canMutate(currentUser: User | Trainer, id: number): Promise<boolean> {
-    if (currentUser instanceof User) return false;
+	async canMutate(
+		currentUser: User | Trainer,
+		id: number
+	): Promise<boolean> {
+		if (currentUser instanceof User) return false
 
-    const sessionHistory = await this.sessionHistoriesRepository.findOneOrFail(
-      id,
-    );
+		const sessionHistory =
+			await this.sessionHistoriesRepository.findOneOrFail(id)
 
-    if (sessionHistory.trainerId !== currentUser.id) {
-      return false;
-    }
+		if (sessionHistory.trainerId !== currentUser.id) {
+			return false
+		}
 
-    return true;
-  }
+		return true
+	}
 }
--- server/src/sessions/sessions.service.ts
+++ server/src/sessions/sessions.service.ts
@@ -1,143 +1,149 @@
 import {
-  BadRequestException,
-  forwardRef,
-  Inject,
-  Injectable,
-} from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { SessionHistoriesService } from 'src/session-histories/session-histories.service';
-import { Trainer } from 'src/trainers/entities/trainer.entity';
-import { TrainersService } from 'src/trainers/trainers.service';
-import SessionStatusTypes from 'src/types/sessionStatus.types';
-import { User } from 'src/users/entities/user.entity';
-import { UsersService } from 'src/users/users.service';
-import { getManager, Repository } from 'typeorm';
-
-import { CreateSessionInput } from './dto/create-session.input';
-import { UpdateSessionInput } from './dto/update-session.input';
-import { Session } from './entities/session.entity';
+	BadRequestException,
+	forwardRef,
+	Inject,
+	Injectable
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { SessionHistoriesService } from 'src/session-histories/session-histories.service'
+import { Trainer } from 'src/trainers/entities/trainer.entity'
+import { TrainersService } from 'src/trainers/trainers.service'
+import SessionStatusTypes from 'src/types/sessionStatus.types'
+import { User } from 'src/users/entities/user.entity'
+import { UsersService } from 'src/users/users.service'
+import { Repository } from 'typeorm'
+import { CreateSessionInput } from './dto/create-session.input'
+import { UpdateSessionInput } from './dto/update-session.input'
+import { Session } from './entities/session.entity'
 
 @Injectable()
 export class SessionsService {
-  constructor(
-    @InjectRepository(Session) private sessionsRepository: Repository<Session>,
-    @Inject(forwardRef(() => UsersService)) private usersService: UsersService,
-    @Inject(forwardRef(() => TrainersService))
-    private trainersService: TrainersService,
-    @Inject(forwardRef(() => SessionHistoriesService))
-    private sessionHistoriesService: SessionHistoriesService,
-  ) {}
+	constructor(
+		@InjectRepository(Session)
+		private sessionsRepository: Repository<Session>,
+		@Inject(forwardRef(() => UsersService))
+		private usersService: UsersService,
+		@Inject(forwardRef(() => TrainersService))
+		private trainersService: TrainersService,
+		@Inject(forwardRef(() => SessionHistoriesService))
+		private sessionHistoriesService: SessionHistoriesService
+	) {}
 
-  create(createSessionInput: CreateSessionInput) {
-    const newSession = this.sessionsRepository.create(createSessionInput);
+	create(createSessionInput: CreateSessionInput) {
+		const newSession = this.sessionsRepository.create(createSessionInput)
 
-    return this.sessionsRepository.save(newSession);
-  }
+		return this.sessionsRepository.save(newSession)
+	}
 
-  findAll(): Promise<Session[]> {
-    return this.sessionsRepository.find();
-  }
+	findAll(): Promise<Session[]> {
+		return this.sessionsRepository.find()
+	}
 
-  findOneById(id: number): Promise<Session> {
-    return this.sessionsRepository.findOneOrFail(id);
-  }
+	findOneById(id: number): Promise<Session> {
+		return this.sessionsRepository.findOneOrFail(id)
+	}
 
-  findAllByTrainerId(trainerId: number): Promise<Session[]> {
-    return this.sessionsRepository.find({
-      where: {
-        trainerId,
-        status: SessionStatusTypes.ACTIVE,
-      },
-      order: {
-        userId: 'ASC',
-      },
-    });
-  }
+	findAllByTrainerId(trainerId: number): Promise<Session[]> {
+		return this.sessionsRepository.find({
+			where: {
+				trainerId,
+				status: SessionStatusTypes.ACTIVE
+			},
+			order: {
+				userId: 'ASC'
+			}
+		})
+	}
 
-  findAllByUserId(userId: number): Promise<Session[]> {
-    return this.sessionsRepository.find({
-      where: {
-        userId,
-        status: SessionStatusTypes.ACTIVE,
-      },
-      order: {
-        userId: 'ASC',
-      },
-    });
-  }
+	findAllByUserId(userId: number): Promise<Session[]> {
+		return this.sessionsRepository.find({
+			where: {
+				userId,
+				status: SessionStatusTypes.ACTIVE
+			},
+			order: {
+				userId: 'ASC'
+			}
+		})
+	}
 
-  getUser(userId: number): Promise<User> {
-    return this.usersService.findOneById(userId);
-  }
+	getUser(userId: number): Promise<User> {
+		return this.usersService.findOneById(userId)
+	}
 
-  getTrainer(trainerId: number): Promise<Trainer> {
-    return this.trainersService.findOneById(trainerId);
-  }
+	getTrainer(trainerId: number): Promise<Trainer> {
+		return this.trainersService.findOneById(trainerId)
+	}
 
-  async update(updateSessionInput: UpdateSessionInput): Promise<Session> {
-    const session = await this.sessionsRepository.findOneOrFail(
-      updateSessionInput.id,
-    );
+	async update(updateSessionInput: UpdateSessionInput): Promise<Session> {
+		const session = await this.sessionsRepository.findOneOrFail(
+			updateSessionInput.id
+		)
 
-    if (session.completedSession && updateSessionInput.completedSession) {
-      throw new BadRequestException('Session is already completed');
-    }
+		if (session.completedSession && updateSessionInput.completedSession) {
+			throw new BadRequestException('Session is already completed')
+		}
 
-    if (
-      'completedSession' in updateSessionInput &&
-      updateSessionInput.completedSession != session.completedSession
-    ) {
-      const { completedSession } = updateSessionInput;
-      let sessionHistory;
-      if (completedSession) {
-        sessionHistory =
-          await this.sessionHistoriesService.findOneToAddUsedCountByUserIdAndTrainerId(
-            session.userId,
-            session.trainerId,
-          );
-      } else {
-        sessionHistory =
-          await this.sessionHistoriesService.findOneToSubtractUsedCountByUserIdAndTrainerId(
-            session.userId,
-            session.trainerId,
-          );
-      }
+		if (
+			'completedSession' in updateSessionInput &&
+			updateSessionInput.completedSession != session.completedSession
+		) {
+			const { completedSession } = updateSessionInput
+			let sessionHistory
+			if (completedSession) {
+				sessionHistory =
+					await this.sessionHistoriesService.findOneToAddUsedCountByUserIdAndTrainerId(
+						session.userId,
+						session.trainerId
+					)
+			} else {
+				sessionHistory =
+					await this.sessionHistoriesService.findOneToSubtractUsedCountByUserIdAndTrainerId(
+						session.userId,
+						session.trainerId
+					)
+			}
 
-      if (!sessionHistory) {
-        throw new BadRequestException('User does not have available session');
-      }
+			if (!sessionHistory) {
+				throw new BadRequestException(
+					'User does not have available session'
+				)
+			}
 
-      await this.sessionHistoriesService.update({
-        id: sessionHistory.id,
-        usedCount: sessionHistory.usedCount + (completedSession ? 1 : -1),
-      });
-    }
-    return this.sessionsRepository.save({
-      ...session,
-      ...updateSessionInput,
-    });
-  }
+			await this.sessionHistoriesService.update({
+				id: sessionHistory.id,
+				usedCount: sessionHistory.usedCount + (completedSession ? 1 : -1)
+			})
+		}
+		return this.sessionsRepository.save({
+			...session,
+			...updateSessionInput
+		})
+	}
 
-  async remove(id: number): Promise<Session> {
-    const session = await this.sessionsRepository.findOneOrFail(id);
-    return this.sessionsRepository.remove(session);
-  }
+	async remove(id: number): Promise<Session> {
+		const session = await this.sessionsRepository.findOneOrFail(id)
+		return this.sessionsRepository.remove(session)
+	}
 
-  /**
-   *
-   * Helpers
-   *
-   */
+	/**
+	 *
+	 * Helpers
+	 *
+	 */
 
-  async canMutate(currentUser: User | Trainer, id: number): Promise<boolean> {
-    const session = await this.findOneById(id);
-    if (currentUser instanceof User) {
-      return session.userId === currentUser.id;
-    } else {
-      // currentUser instanceof Trainer
-      return session.trainerId === currentUser.id;
-    }
-  }
+	async canMutate(
+		currentUser: User | Trainer,
+		id: number
+	): Promise<boolean> {
+		const session = await this.findOneById(id)
+		if (currentUser instanceof User) {
+			return session.userId === currentUser.id
+		} else {
+			// currentUser instanceof Trainer
+			return session.trainerId === currentUser.id
+		}
+	}
 
-  find;
+	find
 }
