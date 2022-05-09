576682b9fc5aa20c17c9dce308a3f236077896e7
Soomin Jang <jsmsumin2@gmail.com>
2022-05-10 07:13:00 +0900

    <base href=/>

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,18 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import logo from '../../public/logo3.svg'
 import Landing from '../components/Landing'
 import Login from '../components/Login'
-import logo from '../../public/logo3.svg'
 
 const Home: NextPage = () => {
 	return (
 		<div className="flex flex-col items-center justify-center box-border m-0 p-0">
 			<Head>
 				<title>Bodysign</title>
+				<base href="/"></base>
 			</Head>
 			<div className="flex">
 				<img src={logo} alt="image" width="50rem" height="50rem" />
-				<div className="text-[3.2rem] text-left font-bold text-[#FDAD00] my-[3rem] mx-[1.5rem]">Bodysign</div>
+				<div className="text-[3.2rem] text-left font-bold text-[#FDAD00] my-[3rem] mx-[1.5rem]">
+					Bodysign
+				</div>
 			</div>
 			<Login />
 			<Landing />
